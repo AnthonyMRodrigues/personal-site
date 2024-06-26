13b72f93ad38a4cdf7a93ca40b195d1625b26b2e
AnthonyMRodrigues <anthonyrodrigues.conceicao+github@gmail.com>
2024-06-25 21:41:10 -0300

    Feature: Using the SES email via SMTP.
    - Using the SES email via SMTP.

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,45 +1,37 @@
+import nodemailer from 'nodemailer';
 import { NextRequest, NextResponse } from 'next/server';
-import { SES, Config } from 'aws-sdk';
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
-    const config = new Config({
-        region: process.env.REGION_AWS,
-    });
-    const ses = new SES({region: process.env.REGION_AWS, credentials: config.credentials});
 
     try {
         const { name, email, message } = await request.json();
 
+        const transporter = nodemailer.createTransport({
+            // @ts-ignore
+            host: process.env.SMTP_EMAIL_ADDRESS, // replace with your SMTP server
+            port: process.env.SMTP_EMAIL_PORT,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: process.env.SMTP_EMAIL_USERNAME, // SMTP user
+                pass: process.env.SMTP_EMAIL_PASSWORD, // SMTP password
+            },
+        });
+
         // Set up email data
-        const params = {
-            Destination: {
-                ToAddresses: [process.env.EMAIL_ADDRESS], // list of receivers
-            },
-            Message: {
-                Body: {
-                    Text: {
-                        Charset: 'UTF-8',
-                        Data: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`, // plain text body
-                    },
-                },
-                Subject: {
-                    Charset: 'UTF-8',
-                    Data: 'New Contact Form Submission', // Subject line
-                },
-            },
-            Source: process.env.SENDER_EMAIL_ADDRESS, // sender address
-            ReplyToAddresses: [process.env.EMAIL_ADDRESS] // list of reply-to addresses
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL_ADDRESS, // sender address
+            to: process.env.EMAIL_ADDRESS, // list of receivers
+            subject: 'New Contact Form Submission', // Subject line
+            text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`, // plain text body
+            replyTo: process.env.EMAIL_ADDRESS, // list of reply-to addresses
         };
 
         // Send email
-        // @ts-ignore
-        await ses.sendEmail(params).promise();
+        await transporter.sendMail(mailOptions);
 
         return NextResponse.json({ message: 'Email sent successfully' }, { status: 200 });
-
     } catch (error) {
         console.log('Error sending email:', (error as Error).message);
-        console.log(ses.config);
-        return NextResponse.json({ error: 'Error sending email', details: (error as Error).message }, { status: 500 });
+        return NextResponse.json({ error: 'Error sending email'}, { status: 500 });
     }
 }
