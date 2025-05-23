276b208593ecb9bf5c8fe26f3c3838bc662020b7
Anthony Rodrigues <anthonyrodrigues.conceicao@gmail.com>
2025-05-23 17:20:32 -0300

    Update: Revise metadata for improved SEO and branding.
    - Updated the title, description, and keywords in the layout.tsx file to better reflect the brand's focus on original fragrances from top brands.
    - Changed the HTML language attribute from 'en' to 'pt-BR' for localization.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,9 +37,9 @@ const futura = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: "Miha Perfumes | Encontre o perfume perfeito para você",
-  description: "Encontre o perfume perfeito para você. Explore nossa seleção de perfumes e descubra novos aromas que combinam com você.",
-  keywords: "perfume, fragrância, perfume recomendado, fragrância recomendada, fragrância para você, fragrância para homem, fragrância para mulher, fragrância para casal, fragrância para família, fragrância para amigos, 212, paco rabanne, chanel, dior, gucci, carolina herrera, calvin klein, versace, yves saint laurent, giorgio armani, prada, burberry, lancome, paco rabanne, jean paul gaultier, hugo boss, bvgari, tom ford",
+  title: "Miha Perfumes | Fragrâncias Originais das Melhores Marcas",
+  description: "Descubra fragrâncias originais diretamente das melhores marcas do mundo. Encontre o perfume perfeito para você com envio para todo o Brasil e atendimento personalizado. Sinta o luxo, viva a essência.",
+  keywords: "perfumes importados, perfumes originais, fragrâncias de luxo, perfumes femininos, perfumes masculinos, perfumes para casal, perfumes recomendados, perfumes famosos, perfumes online, loja de perfumes, Miha Perfumes, 212, Paco Rabanne, Chanel, Dior, Gucci, Carolina Herrera, Calvin Klein, Versace, Yves Saint Laurent, Giorgio Armani, Prada, Burberry, Lancôme, Jean Paul Gaultier, Hugo Boss, Bvlgari, Tom Ford, Abercrombie & Fitch, AFNAN, Al Haramain, Al Wataniah, Amaran, Animale, Antonio Banderas, Armaf, Azzaro, Benetton, Boucheron, Brand Collection, Britney Spears, Brut, Cacharel, Chloé, Coach, Colcci, Corvette, Cuba, Davidoff, Diesel, Disney, Dolce & Gabbana, Emporio Armani, Everlast, Ferrari, Figaro, Forum, Galaxy Concept, Gilles Cantuel, Givenchy, Grès, Gusttavo Lima, iScents, Issey Miyake, Jacques Bogart, Jaguar, Joli Joli, Juicy Couture, Kate Spade, Kenzo, La Florentina, La Rive, Lacoste, Lamborghini, Lanvin, Lattafa, Linn Young, Lolita, Maison Alhambra, Manasik, Marc Jacobs, Marina de Bourbon, Mercedes-Benz, Molyneux, Mont'Anne, MontBlanc, Moschino, Mugler, Nazareno Gabrielli, New Brand, Nina Ricci, Nusuk, Orientica, Paloma Picasso, Paris Elysees, Paris Riviera, Police, Puccini, Puig, Ralph Lauren, Rochas, Saint Hilaire, Shakira, Style & Scents, Ted Lapidus, Tommy Hilfiger, Ulric de Varens, Valentino, Victoria's Secret, Victorinox, Viktor & Rolf, Women'secret, Boutique de Perfumes, Butique de perfumes",
   authors: [{ name: "Miha" }],
   creator: "Anthony Rodrigues",
   publisher: "Anthony Rodrigues",
@@ -49,7 +49,7 @@ export const metadata: Metadata = {
     locale: "pt_BR",
     url: "https://mihaperfumes.com.br", // Replace with your actual domain
     siteName: "Miha Perfumes",
-    title: "Miha Perfumes | Encontre o perfume perfeito para você",
+    title: "Miha Perfumes | Fragrâncias Originais das Melhores Marcas",
     description: "Encontre o perfume perfeito para você. Explore nossa seleção de perfumes e descubra novos aromas que combinam com você.",
     images: [
       {
@@ -61,10 +61,9 @@ export const metadata: Metadata = {
     ],
   },
   twitter: {
-    card: "summary_large_image",
-    title: "Miha - AI-Powered Perfume Recommendations",
-    description: "Discover your perfect perfume with AI-powered recommendations",
-    images: ["/images/miha-logo.jpg"], // Replace with your actual Twitter image
+    title: "Miha Perfumes | Fragrâncias Originais das Melhores Marcas",
+    description: "Descubra fragrâncias originais diretamente das melhores marcas do mundo. Encontre o perfume perfeito para você com envio para todo o Brasil e atendimento personalizado. Sinta o luxo, viva a essência.",
+    images: ["/images/icon.jpeg"], // Replace with your actual Twitter image
     creator: "@miha", // Replace with your Twitter handle
   },
   viewport: "width=device-width, initial-scale=1.0",
@@ -79,7 +78,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="icon" href="/images/icon.jpeg" type="image/jpeg" />
